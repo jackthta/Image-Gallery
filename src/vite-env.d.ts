a849f33d961c2ef125528602599785c786cd2cf6
Jack Ta <jack.th.ta@gmail.com>
2022-11-22 12:34:07 -0800

    feat: add `VITE_*` prefixed env vars interface for env var intellisense/autocomplete
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_UNSPLASH_ACCESS_KEY: string;
+  readonly VITE_UNSPLASH_SECRET_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
