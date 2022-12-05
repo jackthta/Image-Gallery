58fb90b15e55c80dc4f843b4a8543cb4572a4ca4
Jack Ta <jack.th.ta@gmail.com>
2022-12-05 12:22:30 -0800

    feat: make header hero icon reset search bar query
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,10 @@
-import { Dispatch, SetStateAction } from "react";
+import { useRef } from "react";
 
 import SearchBar from "../search-bar/SearchBar";
 
 import hero_icon from "../../../public/aperture.svg";
+
+import type { Dispatch, SetStateAction } from "react";
 
 import "./Header.css";
 
@@ -11,10 +13,26 @@ type Props = {
 };
 
 function Header({ onSearch }: Props) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleHeroIcon = () => {
+    // Searching an empty query string will
+    // search for random photos.
+    onSearch("");
+
+    // Ensure <SearchBar>'s <input> value is empty
+    // after "resetting" search query.
+    // AKA `if (inputRef.current) inputRef.current.value = "";`
+    // (but wanted to try out this syntax just once)
+    inputRef.current && (inputRef.current.value = "");
+  };
+
   return (
     <header className="header">
-      <img className="header__icon" src={hero_icon} alt="Aperture lens" />
-      <SearchBar onSearch={onSearch} />
+      <button className="header__icon-button" onClick={handleHeroIcon}>
+        <img className="header__icon" src={hero_icon} alt="Aperture lens" />
+      </button>
+      <SearchBar ref={inputRef} onSearch={onSearch} />
     </header>
   );
 }
--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,6 +1,13 @@
-import { Dispatch, SetStateAction, KeyboardEvent } from "react";
+import { forwardRef } from "react";
 
 import search_icon from "../../assets/search-icon.svg";
+
+import type {
+  Dispatch,
+  ForwardedRef,
+  KeyboardEvent,
+  SetStateAction,
+} from "react";
 
 import "./SearchBar.css";
 
@@ -8,24 +15,31 @@ type Props = {
   onSearch: Dispatch<SetStateAction<string>>;
 };
 
-const SearchBar = ({ onSearch }: Props) => {
-  const handleSearch = ({ key, target }: KeyboardEvent<HTMLInputElement>) => {
-    if (key === "Enter") {
-      onSearch(target.value);
-    }
-  };
+const SearchBar = forwardRef(
+  ({ onSearch }: Props, ref: ForwardedRef<HTMLInputElement>) => {
+    const handleSearch = ({ key, target }: KeyboardEvent<HTMLInputElement>) => {
+      if (key === "Enter") {
+        onSearch(target.value);
+      }
+    };
 
-  return (
-    <div className="search">
-      <img className="search__icon" src={search_icon} alt="Magnifying glass" />
-      <input
-        className="search__input"
-        type="text"
-        placeholder="Search photos"
-        onKeyDown={handleSearch}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="search">
+        <img
+          className="search__icon"
+          src={search_icon}
+          alt="Magnifying glass"
+        />
+        <input
+          ref={ref}
+          className="search__input"
+          type="text"
+          placeholder="Search photos"
+          onKeyDown={handleSearch}
+        />
+      </div>
+    );
+  }
+);
 
 export default SearchBar;
