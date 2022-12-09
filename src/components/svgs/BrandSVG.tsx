d475d049d15139febe2a9cd32ee226f5a4c14f5d
Jack Ta <jack.th.ta@gmail.com>
2022-12-09 11:57:39 -0800

    fix: `BrandSVG` not respecting CSS set width
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/components/svgs/BrandSVG.tsx
+++ src/components/svgs/BrandSVG.tsx
@@ -9,7 +9,6 @@ function BrandSVG({ className }: SVGProps) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 256 256"
     >
-      <path fill="none" d="M0 0h256v256H0z" />
       <circle
         cx="128"
         cy="128"
